16aa97c20e31487da9f5a46231d86250bca19448
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-08-22 11:53:07 +0700

    tạo màu chủ đạo và các màu phụ, css mặc định cho vài elemet trước

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
     return (
         <div className="App">
             <h1>React App</h1>
+            <button>click hêre</button>
+            <a href="">click here</a>
+            <p>dfsdfsdfsda</p>
         </div>
     );
 }
