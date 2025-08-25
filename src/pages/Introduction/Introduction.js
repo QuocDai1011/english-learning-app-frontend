c8ba19bea72a0abaf0b95317742310ddb0e34877
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-08-25 23:31:46 +0700

    Xây dưng cơ bản  phần about-setion

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -17,31 +17,50 @@ function Introduction() {
 
     return (
         <div className={`${cx('container')}`}>
+            {/* Logo */}
             <Link className={cx('link-home')} to="/home">
                 <img className={cx('logo')} src="logo-link.png" alt="logo" />
             </Link>
+
+            {/* slogan */}
             <p className={cx('slogan')}>
                 "Ở đâu có <span className={cx('slogan-highlight')}>mất gốc tiếng Anh</span> - ở đó có{' '}
                 <span className={cx('slogan-highlight')}>Tre Xanh</span>"
             </p>
+
+            {/* functional button */}
             <div className={cx('box-contain-btn')}>
                 <Link to="/home">
                     <button className={cx('btn-custom')}>Bắt đầu học ngay</button>
                 </Link>
                 <a href="#information">
-                    <button className={cx('btn-custom')}>Xem thêm thông tin</button>
+                    <button className={cx('btn-custom')}>Khám phá khóa học</button>
                 </a>
             </div>
-            <div id="information" className={cx('test')}>
+
+            {/* About Section */}
+            <div className={cx('about-section')}>
+                <h3 className={cx('about-section__heading')}>Giới thiệu về trang web</h3>
+                <p className={cx('about-section__content')}>
+                    Trang web sẽ giúp các bạn hiểu rõ 5 cấu trúc câu cơ bản của tiếng Anh dành cho học sinh, sinh viên và
+                    những người muốn lấy lại căn bản. Đặc biệt, nó sẽ giúp bạn cân hết các bài tập dạng điền loại từ, tiến
+                    bộ ít nhiều trong kĩ năng Writing, hiểu rõ ngữ pháp hơn. Nội dung được thiết kế dễ hiểu, trực quan và
+                    thực tế, kèm quiz sau mỗi bài học cùng bài tập ứng dụng để ghi nhớ nhanh hơn. 
+                    <Link to='/home'> Hãy bắt đầu hành trình học tiếng Anh của bạn ngay hôm nay!</Link>
+                </p>
+            </div>
+
+            {/* informations of lessons */}
+            <div id="information" className={cx('lessons-information')}>
                 <div className="relative border-l-4 border-green-500 ml-6">
                     {steps.map((step, index) => (
                         <motion.div
                             key={step.id}
                             initial={{ opacity: 0, y: 50 }}
                             whileInView={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.5, delay: index * 0.2 }}
+                            transition={{ duration: 0.5, delay: index * 0.3 }}
                             viewport={{ once: false }}
-                            className="mb-8 ml-6"
+                            className="mb-8 ml-6 mt-"
                         >
                             <div className="absolute -left-6 w-10 h-10 bg-green-500 text-white flex items-center justify-center rounded-full">
                                 {step.id}
